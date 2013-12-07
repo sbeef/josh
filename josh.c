c1fbc1073196c0b83086c1f3496c38ca76ff731c
Amanda Strominger <astromin@saruman.cs.oberlin.edu>
2013-12-07 17:09:06 -0500

    jooooooosh

--- josh.c
+++ josh.c
@@ -1,5 +1,23 @@
 #include "josh.h"
 
+void p2(char **args){
+  pid_t child = fork();
+  if (-1 == child){
+    perror("Fork failed");
+    exit(EXIT_FAILURE);
+  }
+  if (child){
+    int status;
+    wait(&status);
+  }
+  else {
+    execvp(args[0], args);
+    perror("Exec failed");
+    exit(EXIT_FAILURE);
+  }
+
+}
+
 char ** parse(char *input) {
   int i;
   char *temp, **args;
@@ -14,18 +32,35 @@ char ** parse(char *input) {
   return args;
 }
 
-int main() {
+/*int main() {
   int c, i;
+  char *prompt = "$";
   char input[MAX_INPUT];
   i = 0;
+  printf("%s ", prompt);
   while (EOF != (c = getchar())) {
     if (i == MAX_INPUT) {
       // handle overflow
-    } else if ('\n' == c) {
+    } 
+    else if ('\n' == c) {
       input[i] = c;
-      parse(input);
+      char **args = parse(input);
       i = 0;
-    } else 
+      printf("%s ", prompt);
+      if (args[1] == NULL){
+        p2(args[0]);
+      }
+    }
+    else 
       input[i] = c;
   }
+}*/
+
+int main(){
+  char **args;
+  args = malloc(sizeof(char *) * 10);
+  args[0] = "echo";
+  args[1] = "hi";
+  args[2] = NULL;
+  p2(args);
 }
--- josh.h
+++ josh.h
@@ -4,6 +4,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 #define MAX_INPUT 1024
 #define DELIM " "
